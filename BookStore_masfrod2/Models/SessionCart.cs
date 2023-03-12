7d93081e25d73eb861d0c197dc4ff5049e94a8d5
5kiing <103718596+5kiing@users.noreply.github.com>
2023-03-12 17:08:59 -0600

    sessioncart model

---
+++ BookStore_masfrod2/Models/SessionCart.cs
@@ -0,0 +1,24 @@
+﻿using BookStore_masfrod2.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_masfrod2.Models
+{
+    public class SessionCart : Cart
+    {
+        public static Cart GetCart (IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            SessionCart cart = session?.GetJson<SessionCart>("Cart") ?? new SessionCart();
+
+            cart.Session = session;
+
+            return cart;
+        }
+    }
+}
